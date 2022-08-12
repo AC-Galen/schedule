ed35b4fa7da40233925a06c63e10a73aadda02f2
AC-Galen <lottaie7359882@gmail.com>
2022-08-12 11:27:51 +0800

    feat: add user modules

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const { route } = require('./routes')
+
+const routes = require('./routes')
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -7,9 +10,7 @@ const PORT = process.env.PORT || 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: ".hbs" }))
 app.set('view engine', 'hbs')
 
-app.get('/', (req,res) => {
-  res.render('index')
-})
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log('App is running on http://localhost:3000')

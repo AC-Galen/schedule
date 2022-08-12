8950816199aa7fb27a89aaf775d37c13754c9f14
AC-Galen <lottaie7359882@gmail.com>
2022-08-12 15:40:25 +0800

    feat: add user login page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ if(process.env.NODE_ENV !== 'production'){
 }
 const express = require('express')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 
 const routes = require('./routes')
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: ".hbs" }))
 app.set('view engine', 'hbs')
+app.use(methodOverride('_method'))
 
 app.use(routes)
 

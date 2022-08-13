32bf72c5d63030678458d21815effad75eb55d9d
AC-Galen <lottaie7359882@gmail.com>
2022-08-13 15:43:16 +0800

    feat: add legister page & express-session

--- app.js
+++ app.js
@@ -2,16 +2,30 @@ if(process.env.NODE_ENV !== 'production'){
   require('dotenv').config()
 }
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 
+
 const routes = require('./routes')
+
+require('./config/mongoose')
 
 const app = express()
 const PORT = process.env.PORT
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: ".hbs" }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.use(methodOverride('_method'))
 
 app.use(routes)
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,12 +1,43 @@
 const express = require('express')
+const User = require('../../models/user')
 const router = express.Router()
 
-
+//登入
 router.get('/login', (req, res) => {
   res.render('login')
-}),
+})
+
+//註冊
 router.get('/register', (req,res) => {
   res.render('register')
 })
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email }).then(user => {
+    if (user) {
+      console.log('User already exist.')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    } else {
+      return User.create({
+        name,
+        email,
+        password
+      })
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+    }
+  })
+  .catch(err => console.log(err))
+})
 
+//登出
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
+})
 module.exports = router

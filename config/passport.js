ffc68fc02be37f5521e9e7f46dcca17355188241
AC-Galen <lottaie7359882@gmail.com>
2022-08-15 21:11:23 +0800

    feat: add google login

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const facebookStrategy = require('passport-facebook').Strategy
+const googleStrategy = require('passport-google-oauth20').Strategy
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
 
@@ -48,6 +49,32 @@ module.exports = app => {
             .catch(err => done(err, false))
         }) 
   }));
+
+  passport.use(new googleStrategy({
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK,
+    profileFields: ['email', 'displayName']
+  },
+    function (request, accessToken, refreshToken, profile, done) {
+      const { name, email } = profile._json
+      User.findOne({ email })
+        .then(user => {
+          if(user) return done(null, user)
+          const randomPassword = Math.random().toString(36).slice(-8)
+          bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(randomPassword, salt))
+            .then(hash => User.create({
+              name,
+              email,
+              password: hash
+            }))
+            .then(user => done(null, user))
+            .catch(err => done(err, false))
+        })
+    }
+  ));
 
   passport.serializeUser((user, done) => {
     done(null, user.id)

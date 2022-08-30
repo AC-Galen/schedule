6281efb5dc94b6b1307f75644ed5ebff92fe6d24
AC-Galen <lottaie7359882@gmail.com>
2022-08-30 22:46:31 +0800

    chore heroku init & add home page time

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Schedule = require('../../models/schedule')
+const User = require('../../models/user')
 const dayjs = require('dayjs')
 
 
@@ -8,9 +9,13 @@ const dayjs = require('dayjs')
 router.get('/', async (req, res) => {
   try {
     const userId = req.user._id
-    const ScheduleData = await Schedule.find({userId}).sort({ _id: 'asc' }).lean()
+    const name = req.user.name
+
+    const ScheduleData = await Schedule.find({ userId }).sort({ _id: 'asc' }).lean()
+    const user = await User.findOne({ name }).lean()
     const date = dayjs().format('YYYY年MM月DD日') 
-    res.render('index', { ScheduleData, date }  )
+    
+    res.render('index', { ScheduleData, date, user }  )
   } catch(err) {
     console.log(err)
   }
